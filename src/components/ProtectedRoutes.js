749e52ab4972379dfcb284f22bb73347a786ef68
Phoomwp <warakorn.pho@everapp.io>
2024-03-06 17:54:15 +0700

    have bug token not found

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -1,43 +1,46 @@
 import React, { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import axios from "axios";
-import  {useSelector,useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { hideLoading, showLoading } from "../redux/features/alertSlice";
 import { setUser } from "../redux/features/userSlice";
 
 export default function ProtectedRoutes({ children }) {
-const dispatch = useDispatch();
-const {user} = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
 
-const getUser = async () => {
-  try {
-    dispatch(showLoading())
-    const res = await axios.post('/api/v1/user/getUserData', 
-    { token : localStorage.getItem('token') },
-    {
-      headers:{
-        Authorization: `Bearer ${localStorage.getItem('token')}`
+  //eslint-disable-next-line
+  const getUser = async () => {
+    try {
+      dispatch(showLoading());
+      const res = await axios.post(
+        "/api/v1/user/getUserData",
+        { token: localStorage.getItem("token") },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (res.data.success) {
+        dispatch(setUser(res.data.data));
+      } else {
+        <Navigate to="/login" />;
+        localStorage.clear();
       }
+    } catch (error) {
+      dispatch(hideLoading());
+      localStorage.clear();
+      console.log(error);
     }
-    )
-    dispatch(hideLoading())
-    if(res.data.success){
-      dispatch(setUser(res.data.data))
-    } else {
-      <Navigate to='/login' />
-    }
-  
-  } catch (error) {
-    dispatch(hideLoading())
-    console.log(error)
-  }
-}
+  };
 
-useEffect(() => {
-  if(!user){
-    getUser()
-  }
-},[user,getUser] )
+  useEffect(() => {
+    if (!user) {
+      getUser();
+    }
+  }, [user, getUser]);
 
   if (localStorage.getItem("token")) {
     return children;
