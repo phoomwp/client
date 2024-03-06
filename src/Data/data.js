4b696d4a240817f878fc42e29780b5146e924828
Phoomwp <warakorn.pho@everapp.io>
2024-03-06 16:05:35 +0700

    F1

---
+++ src/Data/data.js
@@ -0,0 +1,31 @@
+export const SidebarMenu = [
+  {
+    name: "Home",
+    path: "/",
+    icon: "fa-solid fa-house",
+  },
+
+  {
+    name: "Appointments",
+    path: "/appointments",
+    icon: "fa-solid fa-list-ul",
+  },
+
+  {
+    name: "Apply Doctor",
+    path: "/apply-doctor",
+    icon: "fa-solid fa-user-doctor",
+  },
+
+  {
+    name: "Profile",
+    path: "/profile",
+    icon: 'fa-solid fa-user',
+  },
+
+  {
+    name: "Logout",
+    path: "/logout",
+    icon: "fa-solid fa-arrow-right-from-bracket",
+  },
+];
